c4750eeeb63daa5bcbf2119a9acf2092a4de10ee
Geetica <geeticainampudi@gmail.com>
2019-05-18 15:11:55 -0700

    hello-maven-proj

---
+++ src/main/java/com/mypack/hello/Print_Hello_Maven/App.java
@@ -0,0 +1,13 @@
+package com.mypack.hello.Print_Hello_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
